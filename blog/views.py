e02401826cdfa492209e5e5b4ebb7d1a0480e95f
karisslay <karissgetsmail@gmail.com>
2019-07-10 15:40:27 -0700

    Replace whole repo and add buttons.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,11 @@ from django.utils import timezone
 from .models import Post
 
 
-def post_list(request):
+def index(request):
     # return render(request, "blog/post_list.html", {})
 
-    posts = Post.objects.filter(published_date__lte = timezone.now)
-    return render(request, "blog/post_list.html", {})
+    #posts = Post.objects.filter(published_date__lte = timezone.now)
+    return render(request, "blog/index.html", {})
+
+
+
