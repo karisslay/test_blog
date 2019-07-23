4e4b5d1d957dbed7255b9068cee854f875a9b200
karisslay <karissgetsmail@gmail.com>
2019-07-23 15:03:53 -0700

    Adds more updates to the bar buttons.

--- blog/static/bar_bttns.js
+++ blog/static/bar_bttns.js
@@ -13,36 +13,25 @@ var budget_chosen = false;
 var transportation_chosen = false;
 var surprise_chosen = false;
 
-var interval = setInterval(check_if_finished, 1000);
+// var interval = setInterval(check_if_finished, 1000);
 
 var reset_buttons = false;
 
 var dict = null;
 
+const location_button_dict = {
+    "san_francisco": "enabled",
+    "palo_alto": "enabled",
+    "oakland": "enabled",
+    "fremont": "enabled",
+    "walnut_creek": "enabled",
+};
 
+const button_dict_keys = location_button_dict.keys();
 
-
-var location_button_dict = [{
-        
-        "san_francisco": "enabled",
-        "palo_alto": "enabled",
-        "oakland": "enabled",
-        "fremont": "enabled",
-        "walnut_creek": "enabled",
-
-    }];
-
-var button_dict_keys = location_button_dict.keys();
-
-export {location_button_dict}
-export {button_dict_keys}
-
-
-
-
+export { location_button_dict, button_dict_keys };
 
 function set_button_status (button_id, dictionary) {
-    
     console.log("hi1");
     var button_dict_keys = [];
     button_dict_keys.push(dictionary.keys());
@@ -60,79 +49,76 @@ function set_button_status (button_id, dictionary) {
 }
 
 
-function change_button_status (dictionary, button_name, button_id) {
-            
-    // var button_dict_keys = dictionary.keys();
+// function change_button_status(dictionary, button_name, button_id) {
+//     // var button_dict_keys = dictionary.keys();
     
-    for (key of button_dict_keys) {
+//     // for (key of button_dict_keys) {
+//     //     if (reset_buttons == true) {
+//     //         if (dictionary[key] == "disabled") {
+//     //             <a id="{{button_id}}" class="btn bar_btn_custom btn_align_left disable_bttn">
+//     //                 "{{button_name}}"
+//     //             </a>
+//     //         }
+//     //         else if (dictionary[key] == "enabled") {
+//     //             <a id="{{button_id}}" class="btn bar_btn_custom btn_align_left disable_bttn">
+//     //                 "{{button_name}}"
+//     //             </a>
+//     //         }
+//     //     }
+//     // }
 
-        if (reset_buttons == true) {
-            if (dictionary[key] == "disabled") {
-                <a id= {{button_id}} class="btn bar_btn_custom btn_align_left disable_bttn">{{button_name}}</a>
-                
-            }
-
-            else if (dictionary[key] == "enabled") {
-                <a id= {{button_id}} class="btn bar_btn_custom btn_align_left disable_bttn">{{button_name}}</a>
-
-            }
-    }
-            
-    }
-
-    reset_buttons = false;
-}
-
+//     // reset_buttons = false;
+// }
 
 
-var location_button_dict = [];
+// function oak(){
+//     import {location_button_dict} from './bar_bttns.js'
 
-location_button_dict.push = {
+//     import {button_dict_keys} from './bar_bttns.js'
+
+//     src="/static/bar_bttns.js"
+
+//     document.getElementById("oakland_button").onclick = console.log("hi");
+//     document.getElementById("oakland_button").onclick = set_button_status("oakland_button", location_button_dict);
+
+//     change_button_status (this.location_button_dict, oakland_id, Oakland);
+// } 
+
+
+// var location_button_dict = [];
+
+// location_button_dict.push = {
     
-    "san_francisco": "enabled",
-    "palo_alto": "enabled",
-    "oakland": "enabled",
-    "fremont": "enabled",
-    "walnut_creek": "enabled",
+//     "san_francisco": "enabled",
+//     "palo_alto": "enabled",
+//     "oakland": "enabled",
+//     "fremont": "enabled",
+//     "walnut_creek": "enabled",
 
-}
+// }
 
-
-
-
-var budget_button_dict = [];
-
-budget_button_dict.push = {
-
-    "free": "enabled",
-    "five_twenty": "enabled",
-    "twenty_fifty": "enabled",
-    "fifty_plus": "enabled",
-
-}
+// var budget_button_dict = {
+//     "free": "enabled",
+//     "five_twenty": "enabled",
+//     "twenty_fifty": "enabled",
+//     "fifty_plus": "enabled",
+// }
 
 
-var time_button_dict = [];
-
-time_button_dict.push = {
-
-    "2_hours": "enabled",
-    "4_hours": "enabled",
-    "6_hours": "enabled",
-    "6_plus_hours": "enabled",
-
-}
+// var time_button_dict = {
+//     "2_hours": "enabled",
+//     "4_hours": "enabled",
+//     "6_hours": "enabled",
+//     "6_plus_hours": "enabled",
+// }
 
 
-var transportation_button_dict = [];
-
-transportation_button_dict.push = {
-
-    "walking": "enabled",
-    "car": "enabled",
-    "ferry": "enabled",
-    "public_transportation": "enabled",
-}
+// var transportation_button_dict = {
+//     "walking": "enabled",
+//     "car": "enabled",
+//     "ferry": "enabled",
+//     "public_transportation": "enabled",
+// }
 
 
 
