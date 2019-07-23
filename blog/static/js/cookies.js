c88f8df7681ac1c67e32b2770c64034df5aa2c75
Jasmine-C-16 <jasminec54561@gmail.com>
2019-07-23 11:42:26 -0700

    fixes marker, fixes set_type

--- blog/static/js/cookies.js
+++ blog/static/js/cookies.js
@@ -8,5 +8,5 @@ document.cookie = "indoor_activity=false"
 document.cookie = "restaurant=false"
 document.cookie = "cafe=false"
 document.cookie = "shopping=false"
-document.cookie = "museum=false"
+document.cookie = "museum=true"
 
