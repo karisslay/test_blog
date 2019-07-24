454928ca8821581519fab8cec9141f87fc935049
karisslay <karissgetsmail@gmail.com>
2019-07-24 13:46:53 -0700

    Starts saving user data.

--- blog/static/js/cookies.js
+++ blog/static/js/cookies.js
@@ -1,9 +1,3 @@
-//travel modes
-document.cookie = "driving=true"
-document.cookie = "transit=false"
-document.cookie = "bicycling=false"
-document.cookie = "walking=false"
-
 //type of activity
 document.cookie = "outdoor_activity=false"
 document.cookie = "indoor_activity=false"
@@ -12,3 +6,29 @@ document.cookie = "cafe=false"
 document.cookie = "shopping=false"
 document.cookie = "museum=true"
 
+document.cookie = name +"=" + value
+
+//location
+document.cookie = "san_francisco=false"
+document.cookie = "palo_alto=false"
+document.cookie = "oakland=false"
+document.cookie = "fremont=false"
+document.cookie = "walnut_creek=false"
+
+//time
+document.cookie = "two_hours=false"
+document.cookie = "four_hours=false"
+document.cookie = "six_hours=false"
+document.cookie = "six_plus_hours=false"
+
+//budget
+document.cookie = "free=false"
+document.cookie = "five_twenty=false"
+document.cookie = "twenty_fifty=false"
+document.cookie = "fifty_plus=false"
+
+//travel modes
+document.cookie = "driving=true"
+document.cookie = "transit=false"
+document.cookie = "bicycling=false"
+document.cookie = "walking=false"
---
+++ blog/static/js/user_info.js
@@ -0,0 +1,13 @@
+var type = [];
+
+
+var user_info = {
+    
+    "type": type,
+    "location": null,
+    "time": null,
+    "budget": null,
+    "transportation": null,
+
+};
+
