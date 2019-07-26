b33b06624aaca4197372b55ee973fd61cc7e60e5
Jasmine-C-16 <jasminec54561@gmail.com>
2019-07-26 12:12:25 -0700

    it works

--- blog/static/js/cookies.js
+++ blog/static/js/cookies.js
@@ -1,19 +1,3 @@
-//type of activity
-document.cookie = "outdoor_activity=false"
-document.cookie = "indoor_activity=false"
-document.cookie = "restaurant=false"
-document.cookie = "cafe=false"
-document.cookie = "shopping=false"
-document.cookie = "museum=true"
-
-// document.cookie = name +"=" + value
-
-//travel modes
-document.cookie = "driving=true"
-document.cookie = "transit=false"
-document.cookie = "bicycling=false"
-document.cookie = "walking=false"
-
 //locations
 document.cookie = "baylands_nature_preserve=false"
 document.cookie = "mount_sutro_open_space_reserve=false"
